b585ea8126219e2e0f85ac4ed89ed863995b6c0a
Maksym Poskannyi <47009755+max200pl@users.noreply.github.com>
2021-06-16 16:20:14 +0300

    added  service-form style

--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -2,3 +2,4 @@ import "%modules%/header/header";
 import "%modules%/faq-list/faq-list";
 import "%modules%/comment-list-small/comment-list-small";
 import "%modules%/footer/footer";
+import "%modules%/service-form/service-form";
