a02569648a6b579995acfaef147a36f59d85ed69
Maksym Poskannyi <47009755+max200pl@users.noreply.github.com>
2021-06-26 14:39:51 +0300

    fix

--- src/js/index.js
+++ src/js/index.js
@@ -1,33 +1,31 @@
 import "./import/modules";
 import 'regenerator-runtime/runtime'
 
-document.addEventListener('DOMContentLoaded', () =>
+
+const ajaxSend = async (formData) =>
 {
-     const ajaxSend = async (formData) =>
+     const fetchResp = await fetch('/contact', {
+          method: "POST",
+          body: formData
+     });
+     if (!fetchResp.ok) {
+          throw new Error(`статус ошибки ${fetchResp.status}`);
+     }
+     return await fetchResp.text();
+};
+const forms = document.querySelector("form");
+forms.forEach(form =>
+{
+     form.addEventListener('submit', function (e)
      {
-          const fetchResp = await fetch('/contact', {
-               method: "POST",
-               body: formData
-          });
-          if (!fetchResp.ok) {
-               throw new Error(`статус ошибки ${fetchResp.status}`);
-          }
-          return await fetchResp.text();
-     };
-     const forms = document.querySelector("form");
-     forms.forEach(form =>
-     {
-          form.addEventListener('submit', function (e)
-          {
-               e.preventDefault();
-               const formData = new FormData(this);
-               ajaxSend(formData)
-                    .then((response) =>
-                    {
-                         console.log(response);
-                         form.reset();
-                    })
-                    .catch((err) => console.log(err))
-          })
+          e.preventDefault();
+          const formData = new FormData(this);
+          ajaxSend(formData)
+               .then((response) =>
+               {
+                    console.log(response);
+                    form.reset();
+               })
+               .catch((err) => console.log(err))
      })
 })
